175b4b9952fc4c12ab4dd213e210aa320e9d24ab
messuti.edd@gmail.com <messuti.edd@gmail.com@af33bf18-0fca-5c31-2cb0-706f18706882>
2011-05-25 06:59:49 +0000

    Log on

---
+++ jsBatabase/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Test jsDatabase</title>
+		<script type="text/javascript" src="js/jsDataBase.js"></script>
+
+		<script type="text/javascript">
+		jDB.createDB('dbTest');
+		jDB.selDB = 'dbTest';
+		jDB.createTable('usuarios', {cols: ['nombre', 'apellido', 'telefono']});
+
+		jDB.insert('usuarios', {nombre: 'Pepe', apellido: 'Duran', telefono: 23123});
+		jDB.insert('usuarios', {nombre: 'Maria', apellido: 'Melgar', telefono: 233123});
+
+		</script>
+	</head>
+	<body></body>
+</html>
