92ca46461d456854a880ba62210fc2d6846b5fb5
messuti.edd@gmail.com <messuti.edd@gmail.com@af33bf18-0fca-5c31-2cb0-706f18706882>
2011-05-25 20:19:34 +0000

    relaciones 60%

--- jsBatabase/index.php
+++ jsBatabase/index.php
@@ -16,8 +16,10 @@
 		jDB.insert('telefonos', {descr: 'casa', numero: 213212, id_usuarios: 1});
 		jDB.insert('telefonos', {descr: 'trabajo', numero: 2112666, id_usuarios: 1});
 
-		var rows = jDB.select('usuarios').where(function(row) { return row.nombre == 'Pepe' });
-		var pepe = rows.Row(0);
+		var rows = jDB.select('usuarios')
+			.where(function(row) { return row.nombre == 'Pepe' }, 
+			{telefonos: function(fono) {return fono.id == 1}} );
+		var pepe = rows.rows[0];
 		//pepe.apellido = "suarez";
 		//pepe.Save();
 
