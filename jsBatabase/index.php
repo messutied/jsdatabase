66e22e834a2d8a3086fb901781f1e672ba0a5f22
messuti.edd@gmail.com <messuti.edd@gmail.com@af33bf18-0fca-5c31-2cb0-706f18706882>
2011-05-26 20:43:11 +0000

    fixes

--- jsBatabase/index.php
+++ jsBatabase/index.php
@@ -12,14 +12,13 @@
 		jDB.createTable('telefonos', {cols: ['descr', 'numero'], rel: {manyToOne: 'usuarios'}});
 
 		jDB.insert('usuarios', {nombre: 'Pepe', apellido: 'Duran'});
+		jDB.insert('usuarios', {nombre: 'Juan', apellido: 'Duran'});
 		
-		jDB.insert('telefonos', {descr: 'casa', numero: 213212, id_usuarios: 1});
-		jDB.insert('telefonos', {descr: 'trabajo', numero: 2112666, id_usuarios: 1});
+		jDB.insert('telefonos', {descr: 'casa', numero: 213212, id_usuarios: 2});
+		jDB.insert('telefonos', {descr: 'trabajo', numero: 2112666, id_usuarios: 2});
 
-		var rows = jDB.select('usuarios')
-			.where(function(row) { return row.nombre == 'Pepe' }, 
-			{telefonos: function(fono) {return fono.id == 1}} );
-		var pepe = rows.rows[0];
+		var rows = jDB.select('usuarios').withID(2);
+		var pepe = rows.getRow(0);
 		//pepe.apellido = "suarez";
 		//pepe.Save();
 
